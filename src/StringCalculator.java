3ce573a2caf288603dd9210e3fcbc3f07994f2a3
simply-anshul <anshul.surpaithankar.1@gmail.com>
2021-02-24 02:47:27 +0530

    Empty String case

---
+++ src/StringCalculator.java
@@ -0,0 +1,11 @@
+
+public class StringCalculator {
+
+	public static int Add(String inString) {
+		int sum = 0;
+		if(inString == "") {
+			return 0;
+		}
+		return sum;	}
+	
+}
---
+++ src/StringCalculatorTesting.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringCalculatorTesting {
+
+	@Test
+	void test1() {
+		assertEquals(0, StringCalculator.Add(""));
+	}
+
+}
