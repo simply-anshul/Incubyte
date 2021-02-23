383fe3fd73f8c407e7a60038f89ce3721db1c69a
simply-anshul <anshul.surpaithankar.1@gmail.com>
2021-02-24 03:17:11 +0530

    Single, double and multiple input cases

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,10 +2,25 @@
 public class StringCalculator {
 
 	public static int Add(String inString) {
-		int sum = 0;
+		int sum = 0;								  //returning the sum
+        String[] inS = new String[inString.length()]; //for delimiter split string
+        String delimiter = ",";						  //Delimiter Element
+		//For the Empty String return 0
 		if(inString == "") {
 			return 0;
 		}
+		else if(inString.length()==1) {
+			return Integer.parseInt(inString);
+		}
+        for (int i = 0; i < inString.length(); i++) {  
+            inS = inString.split(delimiter);		//Split the elements by delimiter
+        }
+        
+        for (int i = 0; i < inS.length; i++) {
+            sum += Integer.parseInt(inS[i]);
+        }
+
+
 		return sum;	}
 	
 }
--- src/StringCalculatorTesting.java
+++ src/StringCalculatorTesting.java
@@ -5,8 +5,23 @@ import org.junit.jupiter.api.Test;
 class StringCalculatorTesting {
 
 	@Test
-	void test1() {
+	void test1_1() {
+		//Empty String test case
 		assertEquals(0, StringCalculator.Add(""));
 	}
+	
+	@Test
+	void test1_2() {
+		//Single element and 2 element string test case
+		assertEquals(1, StringCalculator.Add("1"));
+		assertEquals(2, StringCalculator.Add("2"));
+		assertEquals(3, StringCalculator.Add("1,2"));
+		assertEquals(5, StringCalculator.Add("2,3"));
+	}
 
+	@Test
+	void test2() {
+		//Unknown number of elements
+		assertEquals(6, StringCalculator.Add("1,2,3"));
+	}
 }
